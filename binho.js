dfecbfd80aa42b08c5a9cb03689f96f02dc0d8c7
Guilherme Guerra <guerra@todocartoes.com.br>
2017-10-12 20:19:41 -0300

    Rewrites Binho using express

--- binho.js
+++ binho.js
@@ -1,15 +1,33 @@
-const telegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 const dotenv = require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 const token = process.env.TELEGRAM_API;
-const bot = new telegramBot(token, {webHook: { port: process.env.PORT }});
-
+const bot = new TelegramBot(token);
 const url = process.env.APP_URL || 'https://binho-bot.herokuapp.com';
+const port = process.env.PORT
 
-// setup webhook
-bot.setWebHook(`${url}/${token}`);
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${url}/bot${token}`);
 
-bot.on('text', (message) => {
+const app = express();
+
+// parse the updates to JSON
+app.use(bodyParser.json());
+
+// We are receiving updates at the route below!
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Start Express Server
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
+});
+
+bot.on('message', (message) => {
   const userId = message.chat.id;
-
-  bot.sendMessage(userId, 'Yo! yo! yo! \u{1F601}');
+  console.log(message)
+  bot.sendMessage(userId, 'Em que posso ajudar? \u{1F601}');
 });

c149bf4b20f80ab68764f9dc7dbcae6e1d60a31e
Guilherme Guerra <guerra@todocartoes.com.br>
2017-10-21 17:19:33 -0200

    Adds zodiac function

--- src/binho.js
+++ src/binho.js
@@ -1,6 +1,5 @@
 import TeleBot from 'telebot'
 import dotenv from 'dotenv'
-import got from 'got'
 import cheerio from 'cheerio'
 import superagent from 'superagent'
 
@@ -9,21 +8,54 @@ const token = process.env.TELEGRAM_API;
 
 const bot = new TeleBot(token)
 
+const zodiac = {
+  "♈ Áries": "aries",
+  "♉ Touro": "touro",
+  "♊ Gêmeos": "gemeos",
+  "♋ Câncer": "cancer",
+  "♌ Leão": "leao",
+  "♍ Virgem": "virgem",
+  "♎ Libra": "libra",
+  "♏ Escorpião": "escorpiao",
+  "♐ Sargitário": "sargitario",
+  "♑ Capricórnio": "capricornio",
+  "♒ Aquário": "aquario",
+  "♓ Peixes": "peixes"
+}
 
 bot.on(/^\/receita (.+)$/, (msg, props) => {
   const term = props.match[1];
-  got(`https://panelinha-api-server-prod.herokuapp.com/v1/search?title=${term}&pageSize=1&pageType=receita`).then(response => {
+  superagent.get(`https://panelinha-api-server-prod.herokuapp.com/v1/search?title=${term}&pageSize=1&pageType=receita`).then(response => {
     console.log(response.body)
   })
 })
 
-bot.on(/^\/signo (.+)$/, (msg, props) => {
-  const term = props.match[1];
+bot.on(/\/signo/, (msg, props) => {
+
+  let replyMarkup = bot.keyboard([
+    [bot.button('♈ Áries'), bot.button('♉ Touro'), bot.button('♊ Gêmeos')],
+    [bot.button('♋ Câncer'),bot.button('♌ Leão'), bot.button('♍ Virgem')],
+    [bot.button('♎ Libra'), bot.button('♏ Escorpião'), bot.button('♐ Sargitário')],
+    [bot.button('♑ Capricórnio'), bot.button('♒ Aquário'), bot.button('♓ Peixes')],
+    ['Sair']
+  ], {resize: true});
+  return bot.sendMessage(msg.from.id, 'Qual seu signo?', {replyMarkup});
+})
+
+bot.on(/(.+)/, (msg, props) => {
+  const term = zodiac[props.match[1]]
   superagent.get(`http://emais.estadao.com.br/horoscopo/${term}`).then(response => {
     let $ = cheerio.load(response.text)
     let horoscopo = $('.h__signo__dia + p').text()
-    return bot.sendMessage(msg.from.id, `Seu horoscopo (${term}): ${horoscopo}`)
+    return bot.sendMessage(msg.from.id, `${msg.from.first_name}, ${horoscopo}`)
   })
 })
+
+// Hide keyboard
+bot.on('Sair', msg => {
+  return bot.sendMessage(
+    msg.from.id, 'Cya!', {replyMarkup: 'hide'}
+  );
+});
 
 bot.start()

0310d6a41664c61c8902fa7d2d200f5a823c9582
Guilherme Guerra <guerrinha@comum.org>
2017-10-22 14:56:39 -0200

    Fixes horoscope funcion

--- src/binho.js
+++ src/binho.js
@@ -9,18 +9,18 @@ const token = process.env.TELEGRAM_API;
 const bot = new TeleBot(token)
 
 const zodiac = {
-  "♈ Áries": "aries",
-  "♉ Touro": "touro",
-  "♊ Gêmeos": "gemeos",
-  "♋ Câncer": "cancer",
-  "♌ Leão": "leao",
-  "♍ Virgem": "virgem",
-  "♎ Libra": "libra",
-  "♏ Escorpião": "escorpiao",
-  "♐ Sargitário": "sargitario",
-  "♑ Capricórnio": "capricornio",
-  "♒ Aquário": "aquario",
-  "♓ Peixes": "peixes"
+  "♈": "aries",
+  "♉": "touro",
+  "♊": "gemeos",
+  "♋": "cancer",
+  "♌": "leao",
+  "♍": "virgem",
+  "♎": "libra",
+  "♏": "escorpiao",
+  "♐": "sagitario",
+  "♑": "capricornio",
+  "♒": "aquario",
+  "♓": "peixes"
 }
 
 bot.on(/^\/receita (.+)$/, (msg, props) => {
@@ -35,26 +35,29 @@ bot.on(/\/signo/, (msg, props) => {
   let replyMarkup = bot.keyboard([
     [bot.button('♈ Áries'), bot.button('♉ Touro'), bot.button('♊ Gêmeos')],
     [bot.button('♋ Câncer'),bot.button('♌ Leão'), bot.button('♍ Virgem')],
-    [bot.button('♎ Libra'), bot.button('♏ Escorpião'), bot.button('♐ Sargitário')],
+    [bot.button('♎ Libra'), bot.button('♏ Escorpião'), bot.button('♐ Sagitário')],
     [bot.button('♑ Capricórnio'), bot.button('♒ Aquário'), bot.button('♓ Peixes')],
-    ['Sair']
+    [bot.button('Sair')]
   ], {resize: true});
   return bot.sendMessage(msg.from.id, 'Qual seu signo?', {replyMarkup});
 })
 
-bot.on(/(.+)/, (msg, props) => {
+bot.on(/^(♈|♉|♊|♋|♌|♍|♎|♏|♐|♑|♒|♓)(.+)$/, (msg, props) => {
   const term = zodiac[props.match[1]]
   superagent.get(`http://emais.estadao.com.br/horoscopo/${term}`).then(response => {
+    let id = msg.from.id
+    let name = msg.from.first_name
     let $ = cheerio.load(response.text)
-    let horoscopo = $('.h__signo__dia + p').text()
-    return bot.sendMessage(msg.from.id, `${msg.from.first_name}, ${horoscopo}`)
+    let horoscope = $('.h__signo__dia + p').text().trim()
+    let day = $('.h__signo__dia').text()
+    return bot.sendMessage( id, `*${day}*\n${name}, ${horoscope}`, {parseMode: 'markdown'} )
   })
 })
 
 // Hide keyboard
-bot.on('Sair', msg => {
+bot.on(/Sair/, msg => {
   return bot.sendMessage(
-    msg.from.id, 'Cya!', {replyMarkup: 'hide'}
+    msg.from.id, 'Até!', {replyMarkup: 'hide'}
   );
 });
 

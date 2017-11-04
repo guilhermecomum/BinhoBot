d49baee1271c7c071d0c151cb4caee35d86058be
Guilherme Guerra <guerrinha@comum.org>
2017-11-04 14:56:19 -0200

    Organizes code

--- src/binho.js
+++ src/binho.js
@@ -1,37 +1,13 @@
 import TeleBot from 'telebot'
 import dotenv from 'dotenv'
-import cheerio from 'cheerio'
-import superagent from 'superagent'
+import getHoroscope from './utils/zodiac'
 
 dotenv.config({ silent: true })
 const token = process.env.TELEGRAM_API;
 
 const bot = new TeleBot(token)
 
-const zodiac = {
-  "♈": "aries",
-  "♉": "touro",
-  "♊": "gemeos",
-  "♋": "cancer",
-  "♌": "leao",
-  "♍": "virgem",
-  "♎": "libra",
-  "♏": "escorpiao",
-  "♐": "sagitario",
-  "♑": "capricornio",
-  "♒": "aquario",
-  "♓": "peixes"
-}
-
-bot.on(/^\/receita (.+)$/, (msg, props) => {
-  const term = props.match[1];
-  superagent.get(`https://panelinha-api-server-prod.herokuapp.com/v1/search?title=${term}&pageSize=1&pageType=receita`).then(response => {
-    console.log(response.body)
-  })
-})
-
 bot.on(/\/signo/, (msg, props) => {
-
   let replyMarkup = bot.keyboard([
     [bot.button('♈ Áries'), bot.button('♉ Touro'), bot.button('♊ Gêmeos')],
     [bot.button('♋ Câncer'),bot.button('♌ Leão'), bot.button('♍ Virgem')],
@@ -39,26 +15,22 @@ bot.on(/\/signo/, (msg, props) => {
     [bot.button('♑ Capricórnio'), bot.button('♒ Aquário'), bot.button('♓ Peixes')],
     [bot.button('Sair')]
   ], {resize: true});
-  return bot.sendMessage(msg.from.id, 'Qual seu signo?', {replyMarkup});
+  return bot.sendMessage(msg.from.id, 'Qual seu signo?', {replyMarkup})
 })
 
-bot.on(/^(♈|♉|♊|♋|♌|♍|♎|♏|♐|♑|♒|♓)(.+)$/, (msg, props) => {
-  const term = zodiac[props.match[1]]
-  superagent.get(`http://emais.estadao.com.br/horoscopo/${term}`).then(response => {
-    let id = msg.from.id
-    let name = msg.from.first_name
-    let $ = cheerio.load(response.text)
-    let horoscope = $('.h__signo__dia + p').text().trim()
-    let day = $('.h__signo__dia').text()
-    return bot.sendMessage( id, `*${day}*\n${name}, ${horoscope}`, {parseMode: 'markdown'} )
-  })
+bot.on(/^(♈|♉|♊|♋|♌|♍|♎|♏|♐|♑|♒|♓)(.+)$/, async (msg, props) => {
+  const term = props.match[1]
+  let id = msg.from.id
+  let name = msg.from.first_name
+  let result = await getHoroscope(term)
+  bot.sendMessage( id, `*${result.day}*\n${name}, ${result.horoscope}`, {parseMode: 'markdown', replayMarkup: 'hide'} )
 })
 
 // Hide keyboard
-bot.on(/Sair/, msg => {
+bot.on(/Sair/, (msg) => {
   return bot.sendMessage(
     msg.from.id, 'Até!', {replyMarkup: 'hide'}
-  );
-});
+  )
+})
 
 bot.start()

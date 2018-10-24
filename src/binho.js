2a6ac8df39a4da4a8665bd2df3b5d5d1aa8bd6f3
Guilherme Guerra <guilherme.guerra@ilegra.com>
2018-10-24 15:37:24 -0300

    Adds a new feature: search album on spotify

--- src/binho.js
+++ src/binho.js
@@ -1,23 +1,27 @@
-import TeleBot from "telebot"
-import dotenv from "dotenv"
-import getHoroscope from "./utils/zodiac"
-import getQuestion from "./utils/questions"
-import getAlbum from "./utils/1001"
+import TeleBot from "telebot";
+import dotenv from "dotenv";
+import getHoroscope from "./utils/zodiac";
+import getQuestion from "./utils/questions";
+import getAlbum from "./utils/1001";
+import { getSpotifyAlbum } from "./utils/spotify";
 
-dotenv.config({ silent: true })
-const token = process.env.TELEGRAM_API
+dotenv.config({ silent: true });
+const token = process.env.TELEGRAM_API;
 
-const bot = new TeleBot(token)
+const bot = new TeleBot({
+  token: token,
+  usePlugins: ["commandButton"]
+});
 
 bot.on(/^\/(start|help|ajuda)$/, async (msg, props) => {
-  let id = msg.from.id
-  let name = msg.from.first_name
+  let id = msg.from.id;
+  let name = msg.from.first_name;
   return bot.sendMessage(
     msg.from.id,
     `Olá ${name},\n\nEu sou o Binho e estou aqui para te ajudar \u{1F916}\n\n - Se quer saber seu horóscopo do dia: /signo\n - Se quiser uma pergunta aleatória: /pergunta\n - Se quiser um album aleatório: /album `,
     { parseMode: "markdown", replayMarkup: "hide" }
-  )
-})
+  );
+});
 
 bot.on(/\/signo/, (msg, props) => {
   let replyMarkup = bot.keyboard(
@@ -37,46 +41,61 @@ bot.on(/\/signo/, (msg, props) => {
       [bot.button("Sair")]
     ],
     { resize: true }
-  )
-  return bot.sendMessage(msg.from.id, "Qual seu signo?", { replyMarkup })
-})
+  );
+  return bot.sendMessage(msg.from.id, "Qual seu signo?", { replyMarkup });
+});
 
 bot.on(/^(♈|♉|♊|♋|♌|♍|♎|♏|♐|♑|♒|♓)(.+)$/, async (msg, props) => {
-  const term = props.match[1]
-  let id = msg.from.id
-  let name = msg.from.first_name
-  let result = await getHoroscope(term)
+  const term = props.match[1];
+  let id = msg.from.id;
+  let name = msg.from.first_name;
+  let result = await getHoroscope(term);
   bot.sendMessage(id, `*${result.day}*\n${name}, ${result.horoscope}`, {
     parseMode: "markdown",
     replayMarkup: "hide"
-  })
-})
+  });
+});
 
 bot.on(/^\/pergunta$/, async (msg, props) => {
-  let id = msg.from.id
-  const question = await getQuestion("random")
-  return bot.sendMessage(msg.from.id, question)
-})
+  let id = msg.from.id;
+  const question = await getQuestion("random");
+  return bot.sendMessage(msg.from.id, question);
+});
 
 bot.on(/^\/pergunta (.+)$/, async (msg, props) => {
-  const question = await getQuestion(props.match[1])
-  return bot.sendMessage(msg.from.id, question)
-})
+  const question = await getQuestion(props.match[1]);
+  return bot.sendMessage(msg.from.id, question);
+});
+
+bot.on(/^\/spotify (.+)$/, async (msg, props) => {
+  const term = props.match[1];
+  const album = await getSpotifyAlbum(term);
+  let markup = ``;
+  if (album) {
+    markup = `${album.external_urls.spotify}`;
+  } else {
+    markup = `Ops! Esse album não existe no spotify =(`;
+  }
+  return bot.sendMessage(msg.from.id, markup, { parseMode: "html" });
+});
 
 bot.on(/^\/album$/, async (msg, props) => {
-  let id = msg.from.id
-  const question = await getAlbum("random")
-  return bot.sendMessage(msg.from.id, question)
-})
+  let id = msg.from.id;
+  const album = await getAlbum("random");
+  const replyMarkup = bot.inlineKeyboard([
+    [
+      bot.inlineButton("🔍 Spotify", {
+        callback: `/spotify ${album}`
+      })
+    ]
+  ]);
 
-bot.on(/^\/album (.+)$/, async (msg, props) => {
-  const question = await getAlbum(props.match[1])
-  return bot.sendMessage(msg.from.id, question)
-})
+  return bot.sendMessage(msg.from.id, album, { replyMarkup });
+});
 
 // Hide keyboard
-bot.on(/Sair/, msg => {
-  return bot.sendMessage(msg.from.id, "Até!", { replyMarkup: "hide" })
-})
+bot.on(/sair/, msg => {
+  return bot.sendMessage(msg.from.id, "Até!", { replyMarkup: "hide" });
+});
 
-bot.start()
+bot.start();
--- src/utils/1001.js
+++ src/utils/1001.js
@@ -1,13 +1,15 @@
-import Albuns from './albumsDB'
+import Albuns from "./albumsDB";
+import { getSpotifyCredentials, getSpotifyAlbum } from "./spotify";
 
 export default function getAlbum(index) {
-
-  const size = Albuns.length
-  const random  = parseInt(Math.random() * (size - 0) + 0)
+  const size = Albuns.length;
+  const random = parseInt(Math.random() * (size - 0) + 0);
 
   if (index && index >= 1 && index <= size) {
-    return Albuns[index - 1]
+    return Albuns[index - 1];
   }
 
-  return Albuns[random]
+  const album = Albuns[random];
+
+  return album;
 }
